c8b92b201cca7d5ecb1ac30028c84fff45f878ac
Guangpeng Wu <Gwu2@mail.greenriver.edu>
2019-02-25 22:00:57 -0500

    jquery doesn't work, the CPU selector doesn't work

---
+++ views/allCPU.php
@@ -0,0 +1,16 @@
+<?php
+
+echo"<div class='col-10 ml-5'>
+    <input class='form-control' name='CPUchoice' list='CPUchoice'>
+</div>
+<datalist id='CPUchoice'>
+    <option value=\"Intel Xeon Platinum 8173M @ 2.00GHz\">
+    <option value=\"Intel Xeon Gold 6154 @ 3.00GHz\">
+    <option value=\"Intel Core i9-7980XE @ 2.60GHz\">
+    <option value=\"Intel Core i7-7820X @ 3.60GHz\">
+    <option value=\"Intel Xeon E5-2680 v3 @ 2.50GHz\">
+    <option value=\"Intel Core i7-7900X @ 3.30GHz\">
+    <option value=\"AMD EPYC 7401P\">
+    <option value=\"Intel Core i7-6900K @ 3.20GHz\">
+
+</datalist>";
