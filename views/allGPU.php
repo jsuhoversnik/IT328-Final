1c34ff97186f5f239bacc97d07c0e05a58ffb71a
Guangpeng Wu <Gwu2@mail.greenriver.edu>
2019-02-26 18:50:28 -0500

    jquery test

---
+++ views/allGPU.php
@@ -0,0 +1,17 @@
+<?php
+
+echo"<div class=\"col-10 ml-5\">
+                            
+                            <input class=\"form-control\" name=\"GPUchoice\" list=\"GPUchoice\">
+                        </div>
+                        <datalist id=\"GPUchoice\">
+                            <option value=\"TITAN Xp COLLECTORS EDITION\">
+                            <option value=\"GeForce GTX 1080 Ti\">
+                            <option value=\"NVIDIA TITAN Xp\">
+                            <option value=\"Radeon Pro Vega 64\">
+                            <option value=\"TITAN V\">
+                            <option value=\"GeForce GTX 1070 Ti\">
+                            <option value=\"GeForce GTX 1080\">
+                            <option value=\"GeForce GTX 980 Ti\">
+
+                        </datalist>";
